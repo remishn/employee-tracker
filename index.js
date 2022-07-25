bcea2bfd7a5695cd9cb79f94407003b55cfa5f35
remishn <nsarem65@gmail.com>
2022-07-25 18:11:26 -0500

    connect to mysql

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const mysql = require('mysql2')
 const inquirer = require('inquirer')
 const consoleTable = require('console.table')
 
+require('dotenv').config()
+
+const connection  = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: process.env.DB_PASSWORD,
+  database: 'employee_db'
+})
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!")
+});
+
 const promptUser = () => {
     inquirer.prompt ([
       {
